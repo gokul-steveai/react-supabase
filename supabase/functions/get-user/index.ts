77e3a6c7a355473b53c43164ac532c5f40e82dbb
Gokul Panwar <gokul.steveai@gmail.com>
2026-03-06 16:46:56 +0530

    Add CORS support and refactor serve function in get-user endpoint

--- supabase/functions/get-user/index.ts
+++ supabase/functions/get-user/index.ts
@@ -1,11 +1,22 @@
 import { createClient } from "npm:@supabase/supabase-js";
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+};
 
 const supabase = createClient(
   Deno.env.get("SUPABASE_URL")!,
   Deno.env.get("SUPABASE_ANON_KEY")!
 );
 
-Deno.serve(async () => {
+serve(async (req) => {
+
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
   const { data, error } = await supabase
     .from("users")
     .select("*");
